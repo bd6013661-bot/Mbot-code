0cc579155b9b289a788353e3fb63cc0d558d9cf0
Neel Bharambe bd6013661 <bd6013661@ahschool.com>
2025-11-24 15:02:47 -0500

    Update sketch_nov14a.c.ino

--- sketch_nov14a.c/sketch_nov14a.c.ino
+++ sketch_nov14a.c/sketch_nov14a.c.ino
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <MeMCore.h>
 
-MeDCMotor motor(M1);
-MeDCMotor motor(M2);
+MeDCMotor motor1(M1);
+MeDCMotor motor2(M2);
 MeRGBLed led(0,30);
 
 void moveForward(){
@@ -10,29 +10,75 @@ led.setColor(0,255,0);
 led.show();
 motor1.run(100);
 motor2.run(-100);
-delay(6000);
+delay(5000);
 }
 
+void moveForward2(){
+  led.setColor(0,255,0);
+  led.show();
+  motor1.run(100);
+  motor2.run(-100);
+  delay(3650);
+  
 }
+
+void waitSeconds(){
+led.setColor(255,0,0);
+led.show();
+motor1.stop();
+motor2.stop();
+delay(3000);
+}
+
 
 void turnRight(){
-  led.setColor(255,0,0);
+  led.setColor(0,255,0);
   led.show();
-  motor1.run(-120);
-  motor2.run(-120);
+  motor1.run(-108);
+  motor2.run(-108);
   delay(560);
   
 }
 
+void turnLeft(){
+  led.setColor(0,255,0);
+  led.show();
+  motor1.run(106);
+  motor2.run(106);
+  delay(560);
+  
+}
+
+void moveBackward(){
+  led.setColor(0,255,0);
+  led.show();
+  motor1.run(-100);
+  motor2.run(100);
+  delay(560);
+  
+}
+void turn180(){
+  led.setColor(0,255,0);
+  led.show();
+  motor1.run(200);
+  motor2.run(200);
+  delay(560);
+  
+}
 void setup(){
   led.setpin(13);
   pinMode(A7,INPUT);
 
   moveForward();
+  waitSeconds();
   turnRight();
+  moveForward2();
+  waitSeconds();
+  turnLeft();
   moveForward();
-  turnRight();
-  moveForward();
+  waitSeconds();
+  moveBackward();
+  turn180();
   motor1.stop();
   motor2.stop();
   
